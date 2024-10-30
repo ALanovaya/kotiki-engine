af6de5b8b3b4039981446bebbb6d1b2b91b2f1f4
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-10-30 22:11:04 +0300

    refactor: move entities outside algorithm

--- src/core/algorithms/algorithm.h
+++ src/core/algorithms/algorithm.h
@@ -8,23 +8,7 @@
 
 namespace algo {
 class Algorithm {
-protected:
-    std::vector<entity::Entity> entities_;
-    mover::Mover* mover_;
-
-    virtual void UpdateStates() = 0;
-
 public:
-    Algorithm(std::vector<entity::Entity> entities, mover::Mover* mover)
-        : entities_(entities), mover_(mover) {};
-
-    void MoveEntities() {
-        mover_->Move(entities_);
-        UpdateStates();
-    }
-
-    virtual std::vector<entity::Entity>& GetEntities() {
-        return entities_;
-    };
+    virtual std::vector<entity::EntityState> GetStates(std::vector<entity::Entity> &entities) = 0;
 };
 }  // namespace algo
