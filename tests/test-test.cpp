dcc12beb37713bbec4329896058e8e32bc09ef6d
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-06 17:33:57 +0300

    feat(build): add build with CMake

---
+++ tests/test-test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+namespace algo {
+namespace {
+TEST(TestTest, TestTest1) {
+    ASSERT_EQ(1 + 2, 3);
+}
+
+TEST(TestTest, TestTest2) {
+    ASSERT_STRNE("Rafik", "Bibizyan");
+}
+}  // namespace
+}  // namespace algo
