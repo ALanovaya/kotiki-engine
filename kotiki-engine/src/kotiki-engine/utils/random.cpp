be6c91a9f6db665c78a5abe629b96d51e4bfe565
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-14 04:00:29 +0300

    move random samplers implementation from header to fix ld error

---
+++ kotiki-engine/src/kotiki-engine/utils/random.cpp
@@ -0,0 +1,9 @@
+#include <kotiki-engine/utils/random.hpp>
+
+double sample_uniform() {
+    static std::random_device rd;
+    static std::mt19937 eng(rd());
+    static std::uniform_real_distribution distr(0.0, 1.0);
+
+    return distr(eng);
+}
