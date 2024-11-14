422ce9820b234ddcc3616892400c762d7ee90952
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-14 03:52:38 +0300

    add random mover implementation

--- kotiki-engine/include/kotiki-engine/core/movers/random_mover.hpp
+++ kotiki-engine/include/kotiki-engine/core/movers/random_mover.hpp
@@ -1,5 +1,13 @@
 #include <kotiki-engine/core/movers/mover.hpp>
 
 namespace mover {
-class RandomMover : public Mover {};
+class RandomMover : public Mover {
+private:
+    double min_step_;
+    double max_step_;
+
+public:
+    RandomMover(double min_step = 0, double max_step = 1);
+    void Move(std::vector<entity::Entity> &entities);
+};
 }  // namespace mover
---
+++ kotiki-engine/src/kotiki-engine/core/movers/random_mover.cpp
@@ -0,0 +1,25 @@
+#include <kotiki-engine/core/movers/random_mover.hpp>
+#include <kotiki-engine/utils/random.hpp>
+#include <stdexcept>
+
+namespace mover {
+RandomMover::RandomMover(double min_step, double max_step) {
+    if (max_step < min_step) {
+        throw std::invalid_argument(
+                "In Random Mover minimal step must be less or equal to maximal step");
+    }
+
+    min_step_ = min_step;
+    max_step_ = max_step;
+}
+
+void RandomMover::Move(std::vector<entity::Entity>& entities) {
+    for (auto& entity : entities) {
+        int x_sign = sample_uniform() > 0.5 ? 1 : -1;
+        int y_sign = sample_uniform() > 0.5 ? 1 : -1;
+
+        entity.x = (min_step_ + sample_uniform() * (max_step_ - min_step_)) * x_sign;
+        entity.y = (min_step_ + sample_uniform() * (max_step_ - min_step_)) * y_sign;
+    }
+}
+}  // namespace mover
