59f9da502c06452966cbcbdebda7721400ee6a27
ALanovaya <lanovaya.al6@gmail.com>
2024-11-17 00:42:09 +0300

    Add moving cats by random_mover

--- kotiki-engine/src/kotiki-engine/main.cpp
+++ kotiki-engine/src/kotiki-engine/main.cpp
@@ -1,5 +1,4 @@
 #include <QApplication>
-#include <QGraphicsEllipseItem>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsScene>
 #include <QGraphicsView>
@@ -8,73 +7,125 @@
 #include <QPixmap>
 #include <QStatusBar>
 #include <QTimer>
-#include <QWheelEvent>
-#include <cmath>
 #include <memory>
+#include <random>
+#include <vector>
 
+#include "kotiki-engine/core/algorithms/naive.hpp"
+#include "kotiki-engine/core/metrics/euclidean.hpp"
+#include "kotiki-engine/core/movers/random_mover.hpp"
 #include "kotiki-engine/graphics/fps_counter.hpp"
 #include "kotiki-engine/graphics/view.hpp"
 
-class MovingPixmap : public QGraphicsPixmapItem {
+class Cats : public QGraphicsPixmapItem {
 public:
-    MovingPixmap(QPixmap const& pixmap, qreal orbitRadius, qreal speed)
-        : QGraphicsPixmapItem(pixmap), orbit_radius_(orbitRadius), speed_(speed), angle_(0) {
-        setTransformOriginPoint(pixmap.width() / 2.0, pixmap.height() / 2.0);
+    Cats(QPixmap const& pixmap, qreal x, qreal y)
+        : QGraphicsPixmapItem(pixmap), target_x_(x), target_y_(y) {
+        setPos(x, y);
     }
 
-    void advance(int phase) {
-        if (!phase) return;
-        angle_ += speed_;
-        setPos(orbit_radius_ * cos(angle_) - pixmap().width() / 2.0,
-               orbit_radius_ * sin(angle_) - pixmap().height() / 2.0);
+    void MoveTo(double x, double y) {
+        target_x_ = x;
+        target_y_ = y;
+    }
+
+    void UpdatePosition() {
+        qreal current_x = pos().x();
+        qreal current_y = pos().y();
+
+        qreal dx = target_x_ - current_x;
+        qreal dy = target_y_ - current_y;
+
+        qreal step_x = dx / 30;
+        qreal step_y = dy / 30;
+
+        setPos(current_x + step_x, current_y + step_y);
+    }
+
+    void UpdatePixmap(QPixmap const& pixmap) {
+        setPixmap(pixmap);
     }
 
 private:
-    qreal orbit_radius_;
-    qreal speed_;
-    qreal angle_;
+    qreal target_x_;
+    qreal target_y_;
 };
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     QMainWindow main_window;
-    main_window.setWindowTitle("Two Cats Orbiting");
-    main_window.resize(800, 600);
+    main_window.setWindowTitle("Random Moving Cats");
+    main_window.resize(1440, 1024);
 
-    QPixmap pixmap("assets/textures/grass.png");
+    QPixmap grass_texture("assets/textures/grass.png");
 
     auto scene = std::make_unique<QGraphicsScene>(&main_window);
-    scene->setSceneRect(-300, -300, 600, 600);
-    scene->setBackgroundBrush(QBrush(pixmap));
+    scene->setSceneRect(-1000, -1000, 2000, 2000);
+    scene->setBackgroundBrush(QBrush(grass_texture));
 
     auto view = std::make_unique<graphics::ResizableGraphicsView>(scene.get());
     main_window.setCentralWidget(view.get());
 
-    // Load images
-    QPixmap image1("assets/textures/pushin.png");
-    QPixmap image2("assets/textures/pushin.png");
+    QPixmap calm_image("assets/textures/pushin.png");
+    QPixmap angry_image("assets/textures/angry_pusheen.png");
+    QPixmap fighting_image("assets/textures/draka_pusheen.png");
 
-    // Create two images
-    auto image_item1 = std::make_unique<MovingPixmap>(image1, 100, 0.05);
-    auto image_item2 = std::make_unique<MovingPixmap>(image2, 200, 0.03);
+    const int cats_size = 50;
 
-    scene->addItem(image_item1.get());
-    scene->addItem(image_item2.get());
+    std::vector<entity::Entity> entities(cats_size);
+    std::vector<std::unique_ptr<Cats>> cats;
 
-    QTimer timer;
-    QObject::connect(&timer, &QTimer::timeout, scene.get(), &QGraphicsScene::advance);
-    timer.start(10);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(-1000, 1000);
+
+    for (int i = 0; i < cats_size; ++i) {
+        entities[i] = {dis(gen), dis(gen)};
+        auto cat = std::make_unique<Cats>(calm_image, entities[i].x, entities[i].y);
+        cats.push_back(std::move(cat));
+        scene->addItem(cats.back().get());
+    }
+
+    mover::RandomMover random_mover(10, 100);
+    algo::NaiveAlgorithm naive_algorithm(200.0, 500.0, new algo::EuclideanMetric);
+
+    QTimer point_timer;
+    QTimer update_timer;
 
     graphics::widgets::FPSCounter fps_counter;
-    QLabel* fps_label = new QLabel(&main_window);
-    main_window.statusBar()->addPermanentWidget(fps_label);
+    auto fps_label = std::make_unique<QLabel>(&main_window);
+    main_window.statusBar()->addPermanentWidget(fps_label.get());
 
-    QObject::connect(&fps_counter, &graphics::widgets::FPSCounter::FpsUpdated,
-                     [fps_label](int fps) { fps_label->setText(QString("FPS: %1").arg(fps)); });
+    QObject::connect(&update_timer, &QTimer::timeout, [&]() {
+        for (size_t i = 0; i < entities.size(); ++i) {
+            cats[i]->UpdatePosition();
+        }
+        fps_counter.FrameRendered();
+    });
 
-    QObject::connect(&timer, &QTimer::timeout, &fps_counter,
-                     &graphics::widgets::FPSCounter::FrameRendered);
+    QObject::connect(&point_timer, &QTimer::timeout, [&]() {
+        random_mover.Move(entities);
+        auto states = naive_algorithm.GetStates(entities);
+        for (size_t i = 0; i < entities.size(); ++i) {
+            cats[i]->MoveTo(entities[i].x, entities[i].y);
+            switch (states[i]) {
+                case entity::EntityState::Calm:
+                    cats[i]->UpdatePixmap(calm_image);
+                    break;
+                case entity::EntityState::Angry:
+                    cats[i]->UpdatePixmap(angry_image);
+                    break;
+                case entity::EntityState::Fighting:
+                    cats[i]->UpdatePixmap(fighting_image);
+                    break;
+            }
+        }
+        fps_label->setText(QString("FPS: %1").arg(fps_counter.GetCurrentFps()));
+    });
+
+    point_timer.start(500);
+    update_timer.start(10);
 
     main_window.show();
 
