ab7e7d771519a0e42a2b658fd58716f37ab14966
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-18 00:37:08 +0300

    fix bug in mover coordinates fixer

--- kotiki-engine/src/kotiki-engine/core/movers/mover.cpp
+++ kotiki-engine/src/kotiki-engine/core/movers/mover.cpp
@@ -3,16 +3,16 @@
 namespace mover {
 void Mover::FixCoordinates(entity::Entity& entity, FieldParams& field_params) {
     while (entity.x < field_params.x) {
-        entity.x += field_params.x;
+        entity.x += field_params.w;
     }
     while (entity.x > field_params.x + field_params.w) {
-        entity.x -= field_params.x + field_params.w;
+        entity.x -= field_params.w;
     }
     while (entity.y < field_params.y) {
-        entity.y += field_params.y;
+        entity.y += field_params.h;
     }
     while (entity.y > field_params.y + field_params.h) {
-        entity.y -= field_params.y + field_params.h;
+        entity.y -= field_params.h;
     }
 }
 }  // namespace mover
