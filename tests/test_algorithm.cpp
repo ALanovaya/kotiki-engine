a51102835ceaca1819fd92058646e1f899ad96ac
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-18 18:09:20 +0300

    add grid lookup algorithm

--- tests/test_algorithm.cpp
+++ tests/test_algorithm.cpp
@@ -65,4 +65,62 @@ TEST(TestNaive, SingleEntityTest) {
     ASSERT_EQ(states.size(), 1);
     ASSERT_EQ(states[0], entity::EntityState::Calm);
 }
+
+TEST(TestGridLookup, FightingOnR0Test) {
+    auto R0 = 1;
+    auto R1 = 10;
+    auto algo = algo::GridLookupAlgorithm(R0, R1, std::make_unique<algo::EuclideanMetric>());
+    entity::EntitiesCollection kotiki({{1.0, 1.0}, {1.0, 1.9}, {3.0, 4.0}}, field_params);
+    auto states = algo.GetStates(kotiki);
+    ASSERT_EQ(states.size(), 3);
+    ASSERT_EQ(states[0], entity::EntityState::Fighting);
+    ASSERT_EQ(states[1], entity::EntityState::Fighting);
+    ASSERT_NE(states[2], entity::EntityState::Fighting);
+}
+
+TEST(TestGridLookup, AllFightingTest) {
+    auto R0 = 1;
+    auto R1 = 10;
+    auto algo = algo::GridLookupAlgorithm(R0, R1, std::make_unique<algo::EuclideanMetric>());
+    entity::EntitiesCollection kotiki({{1.0, 1.0}, {1.3, 1.3}, {1.5, 1.5}}, field_params);
+    auto states = algo.GetStates(kotiki);
+    ASSERT_EQ(states.size(), 3);
+    ASSERT_EQ(states[0], entity::EntityState::Fighting);
+    ASSERT_EQ(states[1], entity::EntityState::Fighting);
+    ASSERT_EQ(states[2], entity::EntityState::Fighting);
+}
+
+TEST(TestGridLookup, NoFightingTest) {
+    auto R0 = 1;
+    auto R1 = 10;
+    auto algo = algo::GridLookupAlgorithm(R0, R1, std::make_unique<algo::EuclideanMetric>());
+    entity::EntitiesCollection kotiki({{1.0, 1.0}, {3.0, 3.0}, {5.0, 5.0}}, field_params);
+    auto states = algo.GetStates(kotiki);
+    ASSERT_EQ(states.size(), 3);
+    ASSERT_NE(states[0], entity::EntityState::Fighting);
+    ASSERT_NE(states[1], entity::EntityState::Fighting);
+    ASSERT_NE(states[2], entity::EntityState::Fighting);
+}
+
+TEST(TestGridLookup, AllCalmTest) {
+    auto R0 = 1;
+    auto R1 = 10;
+    auto algo = algo::GridLookupAlgorithm(R0, R1, std::make_unique<algo::EuclideanMetric>());
+    entity::EntitiesCollection kotiki({{10.0, 10.0}, {30.0, 30.0}, {50.0, 50.0}}, field_params);
+    auto states = algo.GetStates(kotiki);
+    ASSERT_EQ(states.size(), 3);
+    ASSERT_EQ(states[0], entity::EntityState::Calm);
+    ASSERT_EQ(states[1], entity::EntityState::Calm);
+    ASSERT_EQ(states[2], entity::EntityState::Calm);
+}
+
+TEST(TestGridLookup, SingleEntityTest) {
+    auto R0 = 1;
+    auto R1 = 10;
+    auto algo = algo::GridLookupAlgorithm(R0, R1, std::make_unique<algo::EuclideanMetric>());
+    entity::EntitiesCollection kotiki({{69.0, 420.0}}, field_params);
+    auto states = algo.GetStates(kotiki);
+    ASSERT_EQ(states.size(), 1);
+    ASSERT_EQ(states[0], entity::EntityState::Calm);
+}
 }  // namespace
